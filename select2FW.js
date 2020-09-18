77eb82dca7f33e0a4945a24176ed295af944dbc6
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-09-18 16:08:44 +0200

    add css, js and html sample

--- select2FW.js
+++ select2FW.js
@@ -1,6 +1,8 @@
+require('select2/dist/css/select2.min.css');
+require('select2');
 module.exports = function(app){
     var Select2FW = Object.getPrototypeOf(app).Select2FW = new app.Component("select2FW");
-    //Select2FW.debug = true;
+    // Select2FW.debug = true;
     Select2FW.createdAt      = "2.0.0";
     Select2FW.lastUpdate     = "2.0.0";
     Select2FW.version        = "1";
@@ -8,8 +10,47 @@ module.exports = function(app){
     // Select2FW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // Select2FW.requires       = [];
 
-    // Select2FW.prototype.onCreate = function(){
-    // do thing after element's creation
-    // }
+    
+    Select2FW.prototype.onCreate = function(){
+        var select2FW = this;
+        select2FW.$el.wrap('<div class="select2FW-wrapper"></div>');
+        select2FW.classes = select2FW.$el.attr('class');
+        select2FW.select2 = select2FW.$el.select2({
+            minimumResultsForSearch: 5,
+            width: '100%',
+            dropdownParent: select2FW.$el.attr('data-container') == 'body' ? $(document.body) : select2FW.$el.parent(),
+            templateSelection: function(data,container) {
+                if (data.element) {
+                    $(container).addClass($(data.element).attr("class"));
+                }
+                return data.text;
+            },
+            templateResult: function (data, container) {
+                if (data.element) {
+                    $(container).addClass($(data.element).attr("class"));
+                }
+                return data.text;
+            }
+        });
+
+        select2FW.$el.closest('.select2FW-wrapper').find('.select2-container').first().addClass(select2FW.classes);
+
+        if(Select2FW.debug) console.log('Select2FW has been created \n ',select2FW);
+        return select2FW;
+    }
+
+    Select2FW.prototype.onDestroy = function(){
+        var select2FW = this;
+        select2FW.$el.select2('destroy');
+        if(Select2FW.debug) console.log('Select2FW has been destroyed \n ',select2FW);
+    }
+    
+    $(function () {
+        $('select').not('.custom').select2FW();
+        utils.addHtmlHook('select:not(.custom)', function(item){
+            item.select2FW();
+        });
+    });
+
     return Select2FW;
 }
