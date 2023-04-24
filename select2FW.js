f7bd6521d6a0b0d8e291ba5fea4649bd6d2ccdbf
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-04-24 11:35:05 +0200

    bump to 1.2.2

--- select2FW.js
+++ select2FW.js
@@ -4,8 +4,8 @@ module.exports = function(app){
     var Select2FW = Object.getPrototypeOf(app).Select2FW = new app.Component("select2FW");
     // Select2FW.debug = true;
     Select2FW.createdAt      = "2.0.0";
-    Select2FW.lastUpdate     = "2.0.3";
-    Select2FW.version        = "1.2.1";
+    Select2FW.lastUpdate     = "2.1.0";
+    Select2FW.version        = "1.2.2";
     // Select2FW.factoryExclude = true;
     // Select2FW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // Select2FW.requires       = [];
